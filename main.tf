f636c723bd7db7209640e59625e8f9ad7d6e9469
DigvijaySamj <59823094+DigvijaySamj@users.noreply.github.com>
2024-05-19 14:06:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,19 +11,12 @@ resource "aws_vpc" "terraform_vpc" {
   }
 }
 
+
 resource "aws_instance" "terraform-instance-1" {
-  ami                    = "ami-04b70fa74e45c3917"
-  instance_type          = "t2.medium"
-  availability_zone = "us-east-1e"
-  key_name               = "us-east-1"
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.medium"
+
   tags = {
-    Name  = "my-ec2-instance"
-    appid = "terraform"
-    env   = "dev"
+    Name = "HelloWorld"
   }
-
 }
-
-
-
-
